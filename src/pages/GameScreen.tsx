295c9c8498776636e98f1e0f7187666081496a95
Mathijs Baaij <mathijsbaaij@gmail.com>
2023-09-07 12:25:04 -0300

    Add player toggle

--- src/pages/GameScreen.tsx
+++ src/pages/GameScreen.tsx
@@ -1,4 +1,4 @@
-import {createGrid, setGridCellValue} from "../helpers/grid/grid";
+import {checkRowIsSameValue, createGrid, setGridCellValue} from "../helpers/grid/grid";
 import {useCallback, useEffect, useState} from "react";
 import GridItem from "../components/game/GridItem";
 import {GridCellValue} from "../helpers/grid/types";
@@ -6,15 +6,20 @@ import {GridCellValue} from "../helpers/grid/types";
 const GameScreen = () => {
     // set ik local state met initial state van createGrid
     const [grid, setGrid] = useState(createGrid({ size: 3 }));
+    // ts using <> to set type is called generic. In this case player can be 1 or 2
+    const [player, setPlayer] = useState<1 | 2>(1);
 
     useEffect(() => {
         console.log({grid})
+        const isFirstRowTheSame = checkRowIsSameValue({ grid, row: 0, value: 1 }) || checkRowIsSameValue({ grid, row: 0, value: 2 });
+        console.log({isFirstRowTheSame})
     }, [grid]);
 
     const onGridItemClick = useCallback((value: GridCellValue) => {
-        const newGrid = setGridCellValue({...value, value: 1});
+        const newGrid = setGridCellValue({...value, value: player});
         setGrid(newGrid);
-    }, [grid]);
+        setPlayer(player === 1 ? 2 : 1);
+    }, [grid, player]);
 
     return (
         <div className="contgrid">
