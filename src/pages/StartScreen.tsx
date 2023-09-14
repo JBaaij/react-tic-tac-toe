5f079d7dc74ce4f6d92f40fc1fcf591f409bd63b
Joris Baaij <jorisbaaij@Joriss-MacBook-Pro.local>
2023-09-14 15:11:36 +0200

    choose player and if not alert

--- src/pages/StartScreen.tsx
+++ src/pages/StartScreen.tsx
@@ -1,7 +1,7 @@
 import TitleComponent from '../components/TitleComponent';
 import LabelButton from '../components/LabelButton';
 import { useNavigate } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { createGrid, setGridCellValue } from '../helpers/grid/grid';
 import './StartScreen.css';
 import IconX from '../components/icons/IconX';
@@ -11,8 +11,16 @@ const StartScreen = () => {
   const [grid, setGrid] = useState(createGrid({ size: 3 }));
   const [currentPlayer, setCurrentPlayer] = useState(1); // [1, 2]
   const navigate = useNavigate();
+  const [selectedChoice, setSelectedChoice] = useState<string | null>(null);
+  const [alertNoChoice, setAlertNoChoice] = useState(false);
+
   const onPlayGame = () => {
-    navigate('/game');
+    if (selectedChoice === 'ticx' || selectedChoice === 'tico') {
+      navigate('/game');
+    }
+    if (selectedChoice === null) {
+      setAlertNoChoice(true);
+    }
   };
 
   const onNavigateToGame = () => {
@@ -40,25 +48,47 @@ const StartScreen = () => {
       </div>
       <div className="box2">
         <h5 className="firstline">Pick player 1's mark </h5>
-        <form className="box2-buttons">
-          <div id="radiobut" style={{ display: 'flex' }}>
-            <input type="radio" id="ticx" name="choice" />
-            <label htmlFor="ticx">
-              <div style={{ marginLeft: 210 }} />
-              <IconX width={30} height={30} color={'#ccc'} />
-            </label>
-
-            <input type="radio" id="tico" name="choice" />
-            <label htmlFor="tico">
-              <IconO width={30} height={30} color={'#ccc'} />
-            </label>
-          </div>
-        </form>
 
         <h5 className="secondline">Remember : X goes first New Game</h5>
         <form>
           <input type="text" id="nameInput" placeholder="Enter your name" />
           <button id="submitButton">Submit</button>
+        </form>
+        <form className="box2-buttons">
+          <div id="radiobut" style={{ display: 'flex' }}>
+            <input
+              type="radio"
+              id="ticx"
+              name="choice"
+              checked={selectedChoice === 'ticx'}
+              onChange={() => setSelectedChoice('ticx')}
+            />
+            <label htmlFor="ticx">
+              <div style={{ marginLeft: 210 }} />
+              <IconX
+                width={30}
+                height={30}
+                color={selectedChoice === 'ticx' ? '#EDB809' : '#ccc'}
+              />
+            </label>
+
+            <input
+              type="radio"
+              id="tico"
+              name="choice"
+              checked={selectedChoice === 'tico'}
+              onChange={() => setSelectedChoice('tico')}
+            />
+            <label htmlFor="tico">
+              <IconO
+                width={30}
+                height={30}
+                color={selectedChoice === 'tico' ? '#EDB809' : '#ccc'}
+              />
+            </label>
+          </div>
+
+          {alertNoChoice && <p> Please choose player X or O</p>}
         </form>
       </div>
       <LabelButton
